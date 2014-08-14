3d6332ac92f706999193fa43538dfb1dd7e2e620
Roi Avinoam <avinoamr@gmail.com>
2014-08-15 01:49:09 +0300

    Flicker on click

--- actionbar.js
+++ actionbar.js
@@ -1,17 +1,17 @@
 (function ( $ ) {
     $.fn.actionbar = function() {
-        if ( !jQuery.event.special.tap ) return this;
+        var evname = $.event.special.tap ? "tap" : "click";
         return this.each( function ( i, el ) {
             var $this = $( this );
             if ( $this.data( "bs.actionbar" ) ) return;
             $this.data( "bs.actionbar", {} );
 
             var selector = ".actionbar-nav > li > a";
-            $this.on( "tap.bs.actionbar", selector, function ( ev ) {
+            $this.on( evname + ".bs.actionbar", selector, function ( ev ) {
                 var el = $( ev.currentTarget ).css( "opacity", 0.3 );
                 setTimeout(function () { 
                     el.css( "opacity", "" );
-                }, 300 );
+                }, 150 );
             });
             // $this.on( "click.bs.actionbar", selector, onclick );
         });
