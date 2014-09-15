99b381a0beb5ef9f7d3ac3de78a7208ac2c5cd90
Roi Avinoam <avinoamr@gmail.com>
2014-09-15 11:23:53 +0300

    Actionbar with smooth title changes

--- actionbar.js
+++ actionbar.js
@@ -1,19 +1,44 @@
 (function ( $ ) {
-    $.fn.actionbar = function() {
-        var evname = $.event.special.tap ? "tap" : "click";
-        return this.each( function ( i, el ) {
-            var $this = $( this );
-            if ( $this.data( "bs.actionbar" ) ) return;
-            $this.data( "bs.actionbar", {} );
 
-            var selector = ".actionbar-nav > li > a";
-            $this.on( evname + ".bs.actionbar", selector, function ( ev ) {
-                var el = $( ev.currentTarget ).css( "opacity", 0.3 );
-                setTimeout(function () { 
-                    el.css( "opacity", "" );
-                }, 150 );
-            });
-            // $this.on( "click.bs.actionbar", selector, onclick );
+    var replaceWith = $.fn.replaceWith;
+    $.fn.replaceWith = function ( content ) {
+        content = $( content );
+        var that = this;
+        var ev = $.Event( "replace", { 
+            relatedTarget: content[ 0 ],
+            replace: function () {
+                return replaceWith.call( that, content );
+            }
         });
-    };
+        this.trigger( ev );
+        return ev.isDefaultPrevented() ? this : ev.replace();
+    }
+
+    // animate the replacement of titles
+    $( document ).on( "replace", ".actionbar-title", function ( ev ) {
+        var target = $( ev.target );
+        var related = $( ev.relatedTarget );
+        if ( target.hasClass( "right" ) || target.hasClass( "left" ) ) return;
+        ev.preventDefault();
+
+        var direction = related.attr( "data-direction" ) || "left";
+        var reversed = ( direction == "left" ) ? "right" : "left";
+        target.addClass( direction );
+        related
+            .addClass( reversed )
+            .insertAfter( target );
+
+        setTimeout(function () {
+            related.removeClass( reversed );
+            setTimeout( ev.replace, 200 );
+        }, 1 )
+    })
+
+    // flicker on click
+    var evname = ( $.event.special.tap ? "tap" : "click" ) + ".bs.actionbar";
+    var selector = ".actionbar-nav > li > a";
+    $( document ).on( evname, selector, function ( ev ) {
+        var el = $( ev.currentTarget ).css( "opacity", 0.3 );
+        setTimeout(function () { el.css( "opacity", "" ) }, 150 );
+    });
 }( jQuery ));
