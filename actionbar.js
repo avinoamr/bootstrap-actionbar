d51c9e9fda807f98817269070fb12e56e1f18cb3
Roi Avinoam <avinoamr@gmail.com>
2014-09-15 15:43:02 +0300

    Fade enter new actionbar items

--- actionbar.js
+++ actionbar.js
@@ -5,7 +5,7 @@
         content = $( content );
         var that = this;
         var ev = $.Event( "replace", { 
-            relatedTarget: content[ 0 ],
+            relatedTarget: content,
             replace: function () {
                 return replaceWith.call( that, content );
             }
@@ -13,6 +13,21 @@
         this.trigger( ev );
         return ev.isDefaultPrevented() ? this : ev.replace();
     }
+
+    // fade the replacement of links
+    $( document ).on( "replace", ".actionbar-nav > li:not(.actionbar-title)", function ( ev ) {
+        var target = $( ev.target );
+        var related = $( ev.relatedTarget );
+        ev.preventDefault();
+        target.css( "opacity", 0 );
+        related
+            .css( "opacity", 0 )
+            .insertAfter( target );
+        setTimeout( function () {
+            ev.replace();
+            setTimeout( function () { related.css( "opacity", 1 ) }, 1)
+        }, 200 )
+    });
 
     // animate the replacement of titles
     $( document ).on( "replace", ".actionbar-title", function ( ev ) {
@@ -32,7 +47,7 @@
             related.removeClass( reversed );
             setTimeout( ev.replace, 200 );
         }, 1 )
-    })
+    });
 
     // flicker on click
     var evname = ( $.event.special.tap ? "tap" : "click" ) + ".bs.actionbar";
